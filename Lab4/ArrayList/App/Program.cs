44cc3ee775747b30890a4d39e88c020534d1109c
Manar <eng.manar666@gmail.com>
2023-09-21 14:27:00 +0300

    ArrayList.dll

---
+++ Lab4/ArrayList/App/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System;
+namespace App
+{
+    public class Program
+    {
+        static void Main()
+        {
+            ArrayList array = new ArrayList();
+            array.Add(1);
+            array.Add(2);
+            array.Add(3);
+
+            foreach(int i in array)
+            Console.WriteLine(i);
+
+
+            ArrayList array1 = new ArrayList() {"gg","ii"};
+
+            foreach (object i in array1)
+                Console.WriteLine(i);
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Lab4/ArrayList/ArrayList/ArrayList.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+using System;
+public class ArrayList : IEnumerable
+{ 
+    private object[] List;
+    private int count;
+
+    public int capacity
+    {
+        
+        get { return List.Length; } 
+    }
+
+    public int Count 
+    { get => count; set => count = value; }
+
+    public ArrayList(int length=1)
+    {
+        count = 0;
+
+        List = new object[length];
+    }
+
+    public void Add(object item) 
+    { 
+     if (count== List.Length)
+        {
+            Array.Resize(ref List, List.Length*2);
+        }
+        List[count++] = item;
+    }
+
+    public IEnumerator GetEnumerator() 
+    {
+        for (int i = 0;i<count;i++)
+        {
+          yield return List[i];   
+        }
+
+    }
+
+    public object this[int index]
+    { get { return List[index]; }
+        set { List[index] = value; }
+      
+    }
+
+
+    public void Trimitem()
+
+    {
+        object[] temp = new object[count];
+        Array.Copy(List, temp, count);
+        List = temp;
+    }
+
+
+    public void Remove(int index) {
+      for(int i = 0;i<count;i++)
+        {
+            if (i == index)
+            {
+                List[i] = List[i+1];
+            }
+        }
+    
+    
+    
+    }
+    //public class Enumer : IEnumerator
+    //{
+    //    ArrayList list;
+    //    private int index = -1;
+
+    //    public Enumer(ArrayList array)
+    //    { this.list = array; }
+
+    //    public object Current
+    //    {
+
+    //        get {return list.List[index]; }
+    //    }
+
+    //    public bool MoveNext()
+    //    {
+    //        index++;
+    //        return index<list.Count;
+    //    }
+
+    //    public void Reset()
+    //    {
+    //        list.count=0;
+    //    }
+    //}
+
+
+}
+
